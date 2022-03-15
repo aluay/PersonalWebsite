7e044e9193de648e2456ac95ad0286a2e7830027
Abdullah Luay <aluay@gmail.com>
2022-03-15 14:52:34 -0700

    Use CSS classes

--- js/script.js
+++ js/script.js
@@ -35,11 +35,11 @@ window.onload = function gh() {
 
 function insertGHData(data) {
     document.getElementById("description").innerHTML +=
-        `<p>I currently work at ${data[0].company}, Inc. as a <strong>Level 2 IT Support Agent</strong></p>
+        `<p>I currently work at ${data[0].company}, Inc. as a <strong  class="red-1">Level 2 IT Support Agent</strong></p>
         </p><br></b><p class="my-projects">Some projects I coded</p>`;
 
     data[1].forEach(element => {
         document.getElementById("gh").innerHTML +=
-            `<a href=${element.html_url} target="_blank"><div class="project"><p class="project-name"><span>Name</span>${element.name}</p><p class="project-description"><span>Description</span>${element.description}</p><p class="project-technologies-used"><span>Technologies used</span>${element.topics}</p></div></a>`;
+            `<a href=${element.html_url} target="_blank"><div class="project"><p class="project-name"><span  class="red-1">Name</span>${element.name}</p><p class="project-description"><span  class="red-1">Description</span>${element.description}</p><p class="project-technologies-used"><span  class="red-1">Technologies used</span>${element.topics}</p></div></a>`;
     });
 }
