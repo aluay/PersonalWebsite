fa6e0780ea92ceff7a11c6c200ba20f4549a429d
Abdullah Luay <aluay95@gmail.com>
2022-05-05 00:31:27 -0700

    Bug fixes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 document.getElementById("dark-toggle").addEventListener("click", function () {
-    document.getElementById("main").classList.toggle("dark-mode");
+    document.body.classList.toggle("dark-mode");
+    if (document.getElementById("dark-toggle").innerHTML == "Dark") {
+        document.getElementById("dark-toggle").innerHTML = "Light";
+        document.getElementById("dark-toggle").style.backgroundColor = "var(--light-gray)";
+        document.getElementById("dark-toggle").style.color = "var(--gray)";
+    } else {
+        document.getElementById("dark-toggle").innerHTML = "Dark";
+        document.getElementById("dark-toggle").style.backgroundColor = "var(--gray)";
+        document.getElementById("dark-toggle").style.color = "var(--light-gray)";
+    }
 })
